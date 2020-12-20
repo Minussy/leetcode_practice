dbeedbd020b35c6c54e25728912d1f1bcaaaea63
iyangzeshi <iyangzeshi@outlook.com>
2020-12-19 16:24:04 -0800

    origin

--- src/leetcode/editor/en/Leetcode0004MedianOfTwoSortedArrays.java
+++ src/leetcode/editor/en/Leetcode0004MedianOfTwoSortedArrays.java
@@ -29,16 +29,19 @@ package leetcode.editor.en;
 
 // 2020-07-26 14:00:14
 // Zeshi Yang
-public class Leetcode0004MedianOfTwoSortedArrays{
-    // Java: median-of-two-sorted-arrays
-    public static void main(String[] args) {
-        Solution sol = new Leetcode0004MedianOfTwoSortedArrays().new Solution();
-        // TO TEST
-        
-        System.out.println();
-    }
+public class Leetcode0004MedianOfTwoSortedArrays {
+	
+	// Java: median-of-two-sorted-arrays
+	public static void main(String[] args) {
+		Solution sol = new Leetcode0004MedianOfTwoSortedArrays().new Solution();
+		// TO TEST
+		
+		System.out.println();
+	}
+	
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    
     // index:  0   1   2   3   4   5
     //             L1  R1
     // num1:   3   5 | 8   9               4 cut1: 左边有4个元素
@@ -55,7 +58,7 @@ class Solution {
         int cut2;
         int cutL = 0;
         int cutR = nums1.length;
-        while(cutL <= cutR) {
+        while (cutL <= cutR) {
             cut1 = cutL + (cutR - cutL) / 2;
             cut2 = len / 2 - cut1;
             double L1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
@@ -64,16 +67,14 @@ class Solution {
             double R2 = cut2 == len2 ? Integer.MAX_VALUE : nums2[cut2];
             if (L1 > R2) {
                 cutR = cut1 - 1;
-            }
-            else if (L2 > R1) {
+            } else if (L2 > R1) {
                 cutL = cut1 + 1;
             } else {
-                if(len % 2 == 0) {
+                if (len % 2 == 0) {
                     L1 = (Math.max(L1, L2));
                     R1 = (Math.min(R1, R2));
                     return (L1 + R1) / 2;
-                }
-                else {
+                } else {
                     R1 = Math.min(R1, R2);
                     return R1;
                 }
