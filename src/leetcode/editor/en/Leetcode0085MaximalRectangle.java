6224d52231fa123f121fa4ef57ad5d656454c54c
iyangzeshi <iyangzeshi@outlook.com>
2021-01-16 14:46:41 -0800

    origin

--- src/leetcode/editor/en/Leetcode0085MaximalRectangle.java
+++ src/leetcode/editor/en/Leetcode0085MaximalRectangle.java
@@ -1,5 +1,5 @@
-//Given a rows x cols binary matrix filled with 0's and 1's, find the largest re
-//ctangle containing only 1's and return its area. 
+//Given a rows x cols binary matrix filled with 0's and 1's, find the largest
+//rectangle containing only 1's and return its area.
 //
 // 
 // Example 1: 
