059db42fe8c37461421595dd7508b1297813226c
iyangzeshi <iyangzeshi@outlook.com>
2021-01-17 16:34:59 -0800

    origin

--- src/leetcode/editor/en/Leetcode0090SubsetsIi.java
+++ src/leetcode/editor/en/Leetcode0090SubsetsIi.java
@@ -44,52 +44,8 @@ public class Leetcode0090SubsetsIi{
         System.out.println();
     }
 //leetcode submit region begin(Prohibit modification and deletion)
-//Solution 2: BFS 第2类搜索树
 class Solution {
-
-    //Zeshi Yang's code
-    //                                      {}
-    // level 0				    {a}                         {}
-    // level 1          {a, b}          {a}            {b}	    {}
-    // level 2  {a, b, c}   {a, b}  {a,c}   {a}  {b, c}  {b}  {c} {}
-    // null
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        //corner case
-        if (nums == null) {
-            return new ArrayList<>();
-        }
-        Arrays.sort(nums); // important
-        int len = nums.length;
-        Queue<ArrayList<Integer>> queue = new LinkedList<>();
-        ArrayList<Integer> cur = new ArrayList<>();
-
-        int level = 0;
-
-        queue.offer(new ArrayList(cur));
-        // 或者用 (ArrayList) temp.clone()， temp.clone()是shallow copy,返回成Object，再强制转换成ArrayList
-        while (level < len) {
-            int size = queue.size();
-            ArrayList<Integer> prev = null;
-            while (size-- > 0) {
-                cur = queue.poll();
-                cur.add(nums[level]);
-                if (!cur.equals(prev)) {
-                    queue.offer(new ArrayList(cur));
-                }
-                cur.remove(cur.size() - 1);
-                queue.offer(new ArrayList(cur));
-                prev = cur;
-            }
-            level++;
-        }
-        return (List) queue;
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
-
-//Solution 1: BFS 第1类搜索树
-class Solution1 {
-    //Zeshi Yang's code
+    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         //corner case
@@ -100,9 +56,9 @@ class Solution1 {
         int len = nums.length;
         Queue<ArrayList<Integer>> numberQueue = new LinkedList<>();
         Queue<ArrayList<Integer>> indexQueue = new LinkedList<>();
-        ArrayList<Integer> cur = new ArrayList<>();
+        ArrayList<Integer> numList = new ArrayList<>();
         int size;
-        numberQueue.offer(new ArrayList<>(cur));
+        numberQueue.offer(new ArrayList<>(numList));
         indexQueue.offer(new ArrayList<Integer>() {{
                              add(-1);
                          }}
@@ -113,10 +69,11 @@ class Solution1 {
             while (size-- > 0) {
                 ArrayList<Integer> indexList = indexQueue.poll();
                 int index = indexList.get(indexList.size() - 1);
-                cur = numberQueue.poll();
-                res.add(new ArrayList(cur));
+                numList = numberQueue.poll();
+                res.add(new ArrayList<>(numList));
                 // 找到poll出来的结果里面最后一个数字的index，下面的从index + 1的数字开始加
                 int prev = 0;// = index < len - 1 ? nums[index + 1] - 1 : 0;
+                // 从index + 1开始取，如果后面有重复的话，每次值取重复里面的第一个数字
                 for (int i = index + 1; i < len; i++) {
                     if (i == index + 1) {
                         prev = nums[index + 1];
@@ -127,13 +84,75 @@ class Solution1 {
                             prev = nums[i];
                         }
                     }
+                    // 更新索引List
                     indexList.add(i);
-                    indexQueue.offer(new ArrayList(indexList));
+                    indexQueue.offer(new ArrayList<>(indexList));
                     indexQueue.remove(indexQueue.size() - 1);
+                    
+                    // 更新 current number List
+                    numList.add(nums[i]);
+                    numberQueue.offer(new ArrayList<>(numList));
+                    numList.remove(numList.size() - 1);
+                    
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
 
-                    cur.add(nums[i]);
-                    numberQueue.offer(new ArrayList(cur));
-                    cur.remove(cur.size() - 1);
+//Solution 1: BFS 第1类搜索树
+// 6 ms,击败了10.29% 的Java用户,38.9 MB,击败了93.48% 的Java用户
+class Solution1 {
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        //corner case
+        if (nums == null) {
+            return res;
+        }
+        Arrays.sort(nums); // important
+        int len = nums.length;
+        Queue<ArrayList<Integer>> numberQueue = new LinkedList<>();
+        Queue<ArrayList<Integer>> indexQueue = new LinkedList<>();
+        ArrayList<Integer> numList = new ArrayList<>();
+        int size;
+        numberQueue.offer(new ArrayList<>(numList));
+        indexQueue.offer(new ArrayList<Integer>() {{
+                             add(-1);
+                         }}
+        );
+        while (!numberQueue.isEmpty()) {
+            size = numberQueue.size();
+            // cache目前这一层里面有多少个组合
+            while (size-- > 0) {
+                ArrayList<Integer> indexList = indexQueue.poll();
+                int index = indexList.get(indexList.size() - 1);
+                numList = numberQueue.poll();
+                res.add(new ArrayList<>(numList));
+                // 找到poll出来的结果里面最后一个数字的index，下面的从index + 1的数字开始加
+                int prev = 0;// = index < len - 1 ? nums[index + 1] - 1 : 0;
+                // 从index + 1开始取，如果后面有重复的话，每次值取重复里面的第一个数字
+                for (int i = index + 1; i < len; i++) {
+                    if (i == index + 1) {
+                        prev = nums[index + 1];
+                    } else {
+                        if (nums[i] == prev) {
+                            continue;
+                        } else {
+                            prev = nums[i];
+                        }
+                    }
+                    // 更新索引List
+                    indexList.add(i);
+                    indexQueue.offer(new ArrayList<>(indexList));
+                    indexQueue.remove(indexQueue.size() - 1);
+                    
+                    // 更新 current number List
+                    numList.add(nums[i]);
+                    numberQueue.offer(new ArrayList<>(numList));
+                    numList.remove(numList.size() - 1);
 
                 }
             }
@@ -143,8 +162,9 @@ class Solution1 {
 }
 
 //Solution 2: BFS 第2类搜索树
+// 2 ms,击败了34.10% 的Java用户, 39.7 MB,击败了17.26% 的Java用户
 class Solution2 {
-    //Zeshi Yang's code
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         //corner case
         if (nums == null) {
@@ -153,24 +173,27 @@ class Solution2 {
         Arrays.sort(nums); // important
         int len = nums.length;
         Queue<ArrayList<Integer>> queue = new LinkedList<>();
-        ArrayList<Integer> cur = new ArrayList<>();
+        ArrayList<Integer> numList = new ArrayList<>();
 
         int level = 0;
 
-        queue.offer(new ArrayList(cur));
+        queue.offer(new ArrayList<>(numList));
         // 或者用 (ArrayList) temp.clone()， temp.clone()是shallow copy,返回成Object，再强制转换成ArrayList
         while (level < len) {
             int size = queue.size();
             ArrayList<Integer> prev = null;
             while (size-- > 0) {
-                cur = queue.poll();
-                cur.add(nums[level]);
-                if (!cur.equals(prev)) {
-                    queue.offer(new ArrayList(cur));
+                numList = queue.poll();
+                // 加当前这个元素
+                numList.add(nums[level]);
+                if (!numList.equals(prev)) {
+                    queue.offer(new ArrayList<>(numList));
                 }
-                cur.remove(cur.size() - 1);
-                queue.offer(new ArrayList(cur));
-                prev = cur;
+                numList.remove(numList.size() - 1);
+                
+                // 不加当前这个元素
+                queue.offer(new ArrayList<>(numList));
+                prev = numList;
             }
             level++;
         }
@@ -179,8 +202,9 @@ class Solution2 {
 }
 
 //Solution 3: DFS 第1类搜索树
+// 1 ms,击败了99.47% 的Java用户, 38.9 MB,击败了97.64% 的Java用户
 class Solution3 {
-    //Zeshi Yang's code
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         //corner case
@@ -192,9 +216,16 @@ class Solution3 {
         dfs(0, new ArrayList<>(), nums, result);
         return result;
     }
-
-    private void dfs(int idx, List<Integer> list, int[] nums,
-            List<List<Integer>> result) {
+    
+    /**
+     * start from idx to traverse all the possible path and update the results by 1st search tree
+     * @param idx start from idx(inclusively) to go dfs
+     * @param list current path
+     * @param nums given int number array
+     * @param result desired result
+     */
+    private void dfs(int idx, List<Integer> list, int[] nums, List<List<Integer>> result) {
+        // base case
         if (idx == nums.length) {
             return;
         }
@@ -204,19 +235,18 @@ class Solution3 {
                 continue;
             }
             prev = i;
-            list.add(nums[i]);
+            list.add(nums[i]); // update current path list
             result.add(new ArrayList<>(list));
             dfs(i + 1, list, nums, result);
-            // wall
-            list.remove(list.size() - 1);
+            list.remove(list.size() - 1); // restore current path list
         }
-        return;
     }
 }
 
 //Solution 4: DFS 第2类搜索树
+// 1 ms,击败了99.47% 的Java用户, 39 MB,击败了85.89% 的Java用户
 class Solution4 {
-    //Zeshi Yang's code
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         // corner case
@@ -228,10 +258,17 @@ class Solution4 {
 
         return result;
     }
-
-    private void dfs(int idx, int[] nums, List<Integer> list,
-            List<List<Integer>> result) {
+    
+    /**
+     * start from idx to traverse all the possible path and update the results by 2nd search tree
+     * @param idx index that dfs start from
+     * @param nums given int array
+     * @param list current path
+     * @param result the desired result List
+     */
+    private void dfs(int idx, int[] nums, List<Integer> list, List<List<Integer>> result) {
         int len = nums.length;
+        // base case
         if (idx == len) {
             result.add(new ArrayList<>(list));
             return;
